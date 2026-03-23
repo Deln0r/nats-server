c3c79910abf1633bf0b3fc33ed35a4438b50aba1
Maurice van Veen <github@mauricevanveen.com>
2026-03-23 23:04:12 +0100

    WIP: stream move
    Signed-off-by: Maurice van Veen <github@mauricevanveen.com>

--- server/stream.go
+++ server/stream.go
@@ -1147,7 +1147,7 @@ func (mset *stream) streamAssignment() *streamAssignment {
 
 func (mset *stream) setStreamAssignment(sa *streamAssignment) {
 	var node RaftNode
-	var peers []string
+	//var peers []string
 
 	mset.mu.RLock()
 	js := mset.js
@@ -1157,7 +1157,7 @@ func (mset *stream) setStreamAssignment(sa *streamAssignment) {
 		js.mu.RLock()
 		if sa.Group != nil {
 			node = sa.Group.node
-			peers = sa.Group.Peers
+			//peers = sa.Group.Peers
 		}
 		js.mu.RUnlock()
 	}
@@ -1172,9 +1172,9 @@ func (mset *stream) setStreamAssignment(sa *streamAssignment) {
 
 	// Set our node.
 	mset.node = node
-	if mset.node != nil {
-		mset.node.UpdateKnownPeers(peers)
-	}
+	//if mset.node != nil {
+	//	mset.node.UpdateKnownPeers(peers)
+	//}
 
 	// Setup our info sub here as well for all stream members. This is now by design.
 	if mset.infoSub == nil {
